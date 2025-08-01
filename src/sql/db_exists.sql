34ffc8c2e196343be03440ed18ef90f7a8859fc2
maahl <maxence.ahlouche@proton.me>
2025-08-01 22:52:59 +0200

    Fix detection of unitialized db

--- src/sql/db_exists.sql
+++ src/sql/db_exists.sql
@@ -1,9 +1,5 @@
 SELECT EXISTS (
     SELECT *
     FROM information_schema.tables
-    WHERE table_schema = 'public'
-) OR EXISTS (
-    SELECT n.nspname
-    FROM pg_catalog.pg_namespace n
-    WHERE n.nspname !~ '^pg_' AND n.nspname NOT IN ('information_schema', 'public')
+    WHERE table_schema = 'meta' AND table_name = 'migrations'
 );
